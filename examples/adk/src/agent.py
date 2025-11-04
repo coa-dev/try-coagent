92b2c20c815721a5a3255ce890ff55c129337ef0
Leo Borai <estebanborai@gmail.com>
2025-11-04 12:18:02 +0100

    feat: adk update model data

--- examples/adk/src/agent.py
+++ examples/adk/src/agent.py
@@ -1,3 +1,4 @@
+from turtledemo.chaos import N
 import os
 
 from dotenv import load_dotenv
@@ -8,10 +9,12 @@ from .order_processing_tool import order_processing_tool
 load_dotenv()
 
 openai_api_key = os.environ.get("OPENAI_API_KEY")
-openai_model = os.environ.get("OPENAI_MODEL")
+openai_model = os.environ.get("MODEL_GPT_4O")
+
+if openai_api_key is None:
+    raise ValueError("OPENAI_API_KEY environment variable not set")
 
 root_agent = Agent(
-    model=openai_model,
     name='order_processing_agent',
     instruction="Help the user with creating orders, leverage the tools you have access to",
     tools=[order_processing_tool],
